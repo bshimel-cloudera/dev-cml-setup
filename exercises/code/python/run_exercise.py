1def2b72eb8035e363159bd8e0aa783ae9a3ef3c
abhipsagit <162754693+abhipsagit@users.noreply.github.com>
2024-03-12 09:13:19 -0700

    Update run_exercise.py

--- exercises/code/python/run_exercise.py
+++ exercises/code/python/run_exercise.py
@@ -1,1 +1,15 @@
+import subprocess
+import sys
+command = "python3 exercises/code/python/00_cml.py"
 
+try:
+    # Execute the command
+    result = subprocess.run(command, shell=True, check=False)
+
+    print(result)
+    # Check the return code
+    if result.returncode != 0:
+        raise Exception(f"Command failed with return code {result.returncode}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+    sys.exit(result.returncode)
