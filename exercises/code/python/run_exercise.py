d71f5a2082f96c1a1e0bdcfc2b173cc36d4ba4c4
abhipsagit <162754693+abhipsagit@users.noreply.github.com>
2024-03-14 15:40:07 -0700

    Update run_exercise.py

--- exercises/code/python/run_exercise.py
+++ exercises/code/python/run_exercise.py
@@ -1,3 +1,8 @@
-import os
-os.system('python3 exercises/code/python/00_cml.py')
-os.system('python3 exercises/code/python/01_spark.py')
+#import os
+#os.system('python3 exercises/code/python/00_cml.py')
+#os.system('python3 exercises/code/python/01_spark.py')
+import 00_cml
+import 01_spark
+
+00_cml.run()
+01_spark.run()
