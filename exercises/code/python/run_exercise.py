295c568eec2302dc4c4724bf08278cfa249980d8
abhipsagit <162754693+abhipsagit@users.noreply.github.com>
2024-03-14 16:06:06 -0700

    Update run_exercise.py

--- exercises/code/python/run_exercise.py
+++ exercises/code/python/run_exercise.py
@@ -1,8 +1,19 @@
 #import os
 #os.system('python3 exercises/code/python/00_cml.py')
 #os.system('python3 exercises/code/python/01_spark.py')
-import 00_cml
-import 01_spark
 
-00_cml.run()
-01_spark.run()
+import subprocess
+import sys
+command = "bash exercises/code/python/run_exercise.sh"
+
+try:
+    # Execute the command
+    result = subprocess.run(command, shell=True, check=False)
+
+    print(result)
+    # Check the return code
+    if result.returncode != 0:
+        raise Exception(f"Command failed with return code {result.returncode}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+    sys.exit(result.returncode)
